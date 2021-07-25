afca5169038cabf42df417370d559b1dbf9a0ee1
duygutuncer <duygu_tuncer1@hotmail.com>
2021-07-26 01:22:37 +0200

    Part 1 is added. Rendering image will be checked.

--- public/js/script.js
+++ public/js/script.js
@@ -1,20 +1,22 @@
-const vue = require("./vue");
-
-// This is going to contain all of our vue code
-console.log("script is linked");
-
-(function() {
-    new vue({
-        // el is shirt for element
-        // This is how we connect vue with our container #main html
-        el : "#main",
-        data : {
+(function () {
+    new Vue({
+        el: "#main",
+        data: {
             name: "Scallion",
-            seen: true,
-            // if false, you don't see it
-            // looping through
-        
-        }
+            images: [],
+        }, // data ends here
+        mounted: function () {
+            console.log("my vue instance has mounted");
+            console.log("this OUTSIDE of axios: ", this);
+            axios
+                .get("/imageboard")
+                .then((results) => {
+                    console.log("response from /images: ", results.data);
+                    console.log("this INSIDE of axios: ", this);
+                    console.log("this.images:", this.images);
+                    this.images = results.data;
+                })
+                .catch((err) => console.log("err in /imageboard: ", err));
+        },
     });
-
 })();
--- server.js
+++ server.js
@@ -1,6 +1,19 @@
 const express = require("express");
 const app = express();
+const db = require("./db");
 
 app.use(express.static("./public"));
 
-app.listen(8080, () => console.log("IB server is listening"));
+app.get("/imageboard", (req, res) => {
+    db.getImages()
+        .then(({ rows: images }) => {
+            console.log("rows: ", images);
+            res.json(images);
+        })
+        .catch((err) => {
+            console.log("error in /imageboard: ", err);
+            return err;
+        });
+});
+
+app.listen(8080, () => console.log("Listening 8080, this time for imageboard!"));
